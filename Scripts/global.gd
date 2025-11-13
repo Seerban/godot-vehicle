ceab8dd054226a0de25bbeb0a678ee8fa4ffc55e
Seerban <serbanandrei1337@gmail.com>
2025-11-13 14:40:53 +0200

    Added material platforms

--- Scripts/global.gd
+++ Scripts/global.gd
@@ -3,4 +3,8 @@ extends Node
 var material_grip = {
 	load("res://Material/test_material.tres"): 1,
 	load("res://Material/test_material2.tres"): 1,
+	load("res://Material/Grass008_1K-JPG/Grass.tres"): 0.7,
+	load("res://Material/Asphalt025C_1K-JPG/Asphalt.tres"): 1.2,
+	load("res://Material/Ground067_1K-JPG/Ground.tres"): 0.8,
+	load("res://Material/Snow006_1K-JPG/Snow.tres"): 0.6,
 }
