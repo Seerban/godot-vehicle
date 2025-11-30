66ba6e446992f925bf90721befc587c3126b5525
Seerban <serbanandrei1337@gmail.com>
2025-11-30 19:33:33 +0200

    Added sliders implementation

--- UI/car_variables.gd
+++ UI/car_variables.gd
@@ -20,3 +20,50 @@ func _on_turn_slider_value_changed(x: float) -> void:
 	if !check_validity(): return
 	$Roll/Value.text = str(x)
 	for w in car.wheels: w.anti_roll = x
+
+func _on_grip_slider_value_changed(x: float) -> void:
+	if !check_validity(): return
+	$Grip/Value.text = str(x)
+	for w in car.wheels: w.grip = x
+	car.get_node("WheelRR").grip *= 1.2
+	car.get_node("WheelRL").grip *= 1.2
+
+func _on_si_slider_value_changed(x: float) -> void:
+	if !check_validity(): return
+	$SpringInfluence/Value.text = str(x)
+	for w in car.wheels: w.spring_grip_influence = x
+
+func _on_af_slider_value_changed(x: float) -> void:
+	if !check_validity(): return
+	$AccelForgive/Value.text = str(x)
+	for w in car.wheels: w.acceleration_grip_forgiveness = x
+
+func _on_bf_slider_value_changed(x: float) -> void:
+	if !check_validity(): return
+	$BrakeForgive/Value.text = str(x)
+	for w in car.wheels: w.braking_grip_forgiveness = x
+
+func _on_sh_slider_value_changed(x: float) -> void:
+	if !check_validity(): return
+	$SpringHeight/Value.text = str(x)
+	for w in car.wheels: w.spring_length = x
+
+func _on_spring_slider_value_changed(x: float) -> void:
+	if !check_validity(): return
+	$SpringStrength/Value.text = str(x)
+	for w in car.wheels: w.spring_strength = x
+
+func _on_damp_slider_value_changed(x: float) -> void:
+	if !check_validity(): return
+	$Damping/Value.text = str(x)
+	for w in car.wheels: w.damping = x
+
+func _on_aero_slider_value_changed(x: float) -> void:
+	if !check_validity(): return
+	$FrontAero/Value.text = str(x)
+	car.get_node("AeroFront").aero_multiplier = x
+
+func _on_b_aero_slider_value_changed(x: float) -> void:
+	if !check_validity(): return
+	$BackAero/Value.text = str(x)
+	car.get_node("AeroBack").aero_multiplier = x
