662ba328bb6e0397ab8293cf25c6e561ed48f29b
Seerban <serbanandrei1338@gmail.com>
2026-03-31 21:03:45 +0300

    added skybox

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -2,6 +2,20 @@ extends Node3D
 
 @onready var canvas = $CanvasLayer 
 
+func spawn_cars() -> void:
+	var paint_path = "res://Material/Paint/"
+	var paints = ["Candy","Gloss","Matte","Metal","Pearl","Toon"]
+	var offset = $CarSpawnPos.global_position
+	for i in range(len(paints)):
+		var car = load("res://Scenes/vehicle/vehicle.tscn").instantiate()
+		car.get_node("CarMesh").update_material( paints[i] )
+		car.get_node("CarMesh").update_color(Color.INDIAN_RED)
+		add_child(car)
+		car.set_physics_process(false)
+		car.global_position = offset
+		car.global_rotation_degrees.y -= 90
+		offset.x += 10
+
 func flip_car() -> void:
 	var car := $Vehicle
 	car.linear_velocity += Vector3(0, 5, 0)
@@ -10,3 +24,6 @@ func flip_car() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and event.keycode == KEY_F:
 		flip_car()
+
+func _ready() -> void:
+	spawn_cars()
